f44067f38d856601e2ebfd79f21fec7db965df4f
nabil <nabambil689@gmail.com>
2019-10-04 00:45:42 +0800

    Updates

---
+++ precache-manifest.4f19d1f444081f27a4931f5bae06989c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "575b5bcbce7468154d256475655974e0",
+    "url": "/property-admin/index.html"
+  },
+  {
+    "revision": "18c148b7ac853e56c45d",
+    "url": "/property-admin/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "56583e12c0856de5427b",
+    "url": "/property-admin/static/js/2.42f63fb8.chunk.js"
+  },
+  {
+    "revision": "18c148b7ac853e56c45d",
+    "url": "/property-admin/static/js/main.cfa021db.chunk.js"
+  },
+  {
+    "revision": "4deaf6c0dd943a6d53d2",
+    "url": "/property-admin/static/js/runtime-main.325444a6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/property-admin/static/media/logo.25bf045c.svg"
+  }
+]);
